7d88103044a5a91cdc435edf18cbb58470133e78
codermominur360 <mominur19982016@gmail.com>
2019-09-09 22:55:52 +0600

    blog and afiliate

---
+++ wp-content/plugins/cf7-store-to-db-lite/includes/view/about.php
@@ -0,0 +1,135 @@
+<?php defined('ABSPATH') or die("No script kiddies please!"); ?>
+<div class="cd7stdb-wrapper cd7stdb-clear">
+    <div class="cd7stdb-head">     
+        <?php include(CF7STDBL_PATH . 'includes/view/header.php'); ?> 
+    </div> 
+    <div class="cd7stdb-inner-wrapper" id="poststuff">
+        <div id="post-body-full" class="metabox-holder columns-2">
+            <div id="post-body-content">
+                <div class="postbox">
+                    <div class="cd7stdb-menu-option-wrapper clearfix" id="col-container">
+                        <div class="inside" id="cd7stdb-reports-setting-wrapper">
+                            <div class="cd7stdb-header-title cd7stdb-menu-option-header-title">
+                                <h3><?php _e('About Plugin', CF7STDBL_TXT_DOMAIN); ?></h3>
+                            </div>
+                            <div class="cf7stdbl-tab-wrapper">
+                                <p><strong><?php _e('Contact Form 7 Store to DB Lite', CF7STDBL_TXT_DOMAIN) ?></strong><?php _e(' - is the free utility plugin for WordPress to store entries of Contact Form 7 to database with minimal setting and configuration possible.', CF7STDBL_TXT_DOMAIN); ?></p>
+                                <hr />
+                                <h3><?php _e('About AccessPress Themes', CF7STDBL_TXT_DOMAIN); ?></h3>
+                                <p><?php _e('AccessPress Themes is a venture of Access Keys - who has developed hundreds of Custom WordPress themes and plugins for its clients over the years. ', CF7STDBL_TXT_DOMAIN); ?></p>
+                                <div class="cf7stdbl-halfseperator"></div>
+                                <p><strong><?php _e('Please visit our product page for more details here:', CF7STDBL_TXT_DOMAIN); ?></strong>
+                                    <br />
+                                    <a href="https://accesspressthemes.com/wordpress-plugins/contact-form-7-store-to-db-lite/" target="_blank">https://accesspressthemes.com/wordpress-plugins/contact-form-7-store-to-db-lite/</a>
+                                </p>
+                                <div class="cf7stdbl-halfseperator"></div>
+                                <p><strong><?php _e('Please visit our demo page here:', CF7STDBL_TXT_DOMAIN); ?></strong>
+                                    <br />
+                                    <a href="https://demo.accesspressthemes.com/wordpress-plugins/total-team-lite/contact-form-7-store-to-db-lite" target="_blank">https://demo.accesspressthemes.com/wordpress-plugins/contact-form-7-store-to-db-lite/</a>
+                                </p>
+
+                                <div class="cf7stdbl-halfseperator"></div>
+                                <p><strong><?php _e('Plugin documentation can be found here:', CF7STDBL_TXT_DOMAIN); ?></strong>
+                                    <br />
+                                    <a href="https://accesspressthemes.com/documentation/contact-form-7-store-to-db-lite/" target="_blank">https://accesspressthemes.com/documentation/contact-form-7-store-to-db-lite/</a>
+                                </p>
+                                <p>&nbsp;</p>
+                                <hr />
+                                <h3 class="cf7stdbl-sub-title">More from AccessPress themes </h3>
+                                <div class="cf7stdbl-row clearfix">
+                                    <div class="cf7stdbl-col-one-third">
+                                        <div class="cf7stdbl-product">
+                                            <div class="cf7stdbl-logo-product">
+                                                <a href="https://accesspressthemes.com/plugins/" target="_blank">
+                                                    <img src="<?php echo CF7STDBL_IMG_DIR; ?>/plugin.png" alt="<?php esc_attr_e('AccessPress Social Icons', CF7STDBL_TXT_DOMAIN); ?>" />
+                                                </a>
+                                            </div>
+                                            <div class="cf7stdbl-productext">
+                                                <p><strong>WordPress Plugins</strong>
+                                                    <br />
+                                                    <a href="https://accesspressthemes.com/plugins/" target="_blank">https://accesspressthemes.com/plugins/</a>
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="cf7stdbl-col-one-third">
+                                        <div class="cf7stdbl-product">
+                                            <div class="cf7stdbl-logo-product">
+                                                <a href="https://accesspressthemes.com/themes/" target="_blank"><img src="<?php echo CF7STDBL_IMG_DIR; ?>/theme.png" /></a>
+                                            </div>
+                                            <div class="cf7stdbl-productext">
+                                                <p><strong>WordPress Themes</strong>
+                                                    <br />
+                                                    <a href="https://accesspressthemes.com/themes/" target="_blank">https://accesspressthemes.com/themes/</a>
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="cf7stdbl-col-one-third">
+                                        <div class="cf7stdbl-product">
+                                            <div class="cf7stdbl-logo-product">
+                                                <a href="https://accesspressthemes.com/contact/" target="_blank"><img src="<?php echo CF7STDBL_IMG_DIR; ?>/customize.png" /></a>
+                                            </div>
+                                            <div class="cf7stdbl-productext">
+                                                <p><strong>WordPress Customization</strong>
+                                                    <br />
+                                                    <a href="https://accesspressthemes.com/contact/" target="_blank">https://accesspressthemes.com/contact/</a>
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <hr />
+                                <h3 id="item-description__get-latest-updates">More WordPress Resources</h3>
+                                <div>
+                                    <p> Themes Compatible with the Plugin :</p>
+
+                                    <p><strong>Contact Form 7 Store to DB Lite </strong> works best with every WordPress theme. It's even more remarkable when used with popular themes like VMagazine and AccessPress Parallax.</p>
+
+                                    <p>AND IF THIS PLUGIN HAS IMPRESSED YOU, THEN YOU WOULD ENJOY OUR OTHER PROJECTS TOO. DO CHECK THESE OUT :</p>
+
+                                    <p><a href="https://wpall.club/">WPAll Club</a> -  A complete WordPress resources club. WordPress tutorials, blogs, curated free and premium themes and plugins, WordPress deals, offers, hosting info and more.</p>
+
+                                    <p> <a href="https://themeforest.net/user/accesskeys/portfolio">Premium WordPress Themes</a> -   6 premium WordPress themes well suited for all sort of websites. Professional, well coded and highly configurable themes for you. </p>
+
+                                    <p>  <a href="https://codecanyon.net/user/accesskeys/portfolio?Ref=AccessKeys">Premium WordPress Plugins</a> - 45+ premium WordPress plugins of many different types. High user ratings, great quality and best sellers in CodeCanyon marketplace. </p>
+
+                                    <p>  <a href="https://accesspressthemes.com/">AccessPress Themes</a> - AccessPress Themes has 50+ beautiful and elegant, fully responsive, multipurpose themes to meet your need for free and commercial basis.</p>
+
+                                    <p>  <a href="https://8degreethemes.com/">8Degree Themes</a> - 8Degree Themes offers 15+ free WordPress themes and 16+ premium WordPress themes carefully crafted with creativity.</p>
+                                </div>
+                                <h3><?php _e('Get in touch', CF7STDBL_TXT_DOMAIN); ?></h3>
+                                <p><?php _e('If you have any question/feedback, please get in touch:', CF7STDBL_TXT_DOMAIN); ?></p>
+                                <p>
+                                    <strong>Support:</strong> <a href="https://accesspressthemes.com/support/">https://accesspressthemes.com/support/</a><br />
+                                    <strong>General enquiries:</strong> <a href="mailto:info@accesspressthemes.com">info@accesspressthemes.com</a><br />
+                                    <strong>Sales:</strong> <a href="mailto:sales@accesspressthemes.com">sales@accesspressthemes.com</a>
+                                </p>
+                            </div>
+                        </div>
+
+                        <hr />
+                        <div class="cf7stdbl-col-three-third">
+                            <h3><?php _e('Get social', CF7STDBL_TXT_DOMAIN); ?></h3>
+                            <p><?php _e('Get connected with us on social media. Facebook is the best place to find updates on our themes/plugins: ', CF7STDBL_TXT_DOMAIN); ?></p>
+                            <p><strong>Like us on facebook:</strong>
+                                <br />
+                                <iframe style="border: none; overflow: hidden; width: 700px; height: 250px;" src="//www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2Fpages%2FAccessPress-Themes%2F1396595907277967&amp;width=842&amp;height=258&amp;colorscheme=light&amp;show_faces=true&amp;header=false&amp;stream=false&amp;show_border=true&amp;appId=1411139805828592" width="240" height="150" frameborder="0" scrolling="no"></iframe>
+                            </p>
+                            <ul class="cf7stdbl-about cf7stdbl-unstyled cf7stdbl-inlinelist clearfix">
+                                <li><a href="https://plus.google.com/u/0/+Accesspressthemesprofile/about" target="_blank"><img src="<?php echo CF7STDBL_IMG_DIR; ?>/googleplus.png" alt="google+"></a>
+                                </li>
+                                <li><a href="https://www.pinterest.com/accesspresswp/" target="_blank"><img src="<?php echo CF7STDBL_IMG_DIR; ?>/pinterest.png" alt="pinterest"></a>
+                                </li>
+                                <li><a href="https://www.flickr.com/photos/accesspressthemes/" target="_blank"><img src="<?php echo CF7STDBL_IMG_DIR; ?>/flicker.png" alt="flicker"></a>
+                                </li>
+                                <li><a href="https://twitter.com/apthemes" target="_blank"><img src="<?php echo CF7STDBL_IMG_DIR; ?>/twitter.png" alt="twitter"></a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ wp-content/plugins/cf7-store-to-db-lite/readme.txt
@@ -0,0 +1,138 @@
+﻿=== Contact Form 7 Store to DB Lite - Contact Form 7 Extension to Store Form Entries ===
+Contributors: Access Keys
+Tags: contact form 7, contact form, cf7, contact, csv, csv export, forms, storage, database, store to db, capture
+Donate link: http://accesspressthemes.com/donation/
+Requires at least: 3.8
+Tested up to: 5.2
+Stable tag: 1.0.4
+License: GPLv2 or later
+Requires PHP: 5.4.3
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Plugin to easy store and manage contact form 7 entries.
+
+== Description ==
+
+[Homepage](https://accesspressthemes.com/) | [Documentation](https://accesspressthemes.com/documentation/contact-form-7-store-to-db-lite/) | [Support](https://accesspressthemes.com/support) | [Demo](http://demo.accesspressthemes.com/wordpress-plugins/contact-form-7-store-to-db-lite/)
+
+
+<strong>Contact Form 7 Store To DB Lite</strong> - is the extension/addon to the contact form 7 plugin used to store all the submission entries submitted via the contact form 7
+without losing all the data including the attachments. Entries from the contact form 7 plugin will be stored safely even if the form failed to get submitted or any of your email get lost, deleted or removed by mistake.
+
+<strong>Protect, Manage and store the Contact Form 7 entries of your business which are important to you using the plugin.</strong>
+
+= Features: =
+
+* <strong>Custom Post Type Entries</strong>
+ 	- Custom post type for easy handling of the contact form 7 entries.
+* <strong>Attachment Storage in Default WordPress Gallery</strong>
+ 	- When the attachment is submitted through contact form 7 plugin, plugin store the attachment in the default WordPress Gallery.
+* <strong>Delete Attachment While Deleting Entries</strong>
+	- Ability to set if attachment related with the specific post will also be deleted.
+* <strong>Enable Direct Delete of Entries Post</strong>
+	- Ability to set if trashing the entries will skip trash and permanently delete the entries.
+* <strong>Dedicated Support</strong>
+        - Get free email and forum support.
+* <strong>Translation Ready</strong>
+* <strong>WordPress Compatible</strong>
+* <strong>Fully Responsive</strong>
+ 	- Compatible with different devices.
+* <strong>Free updates</strong>
+        - Get free updates for lifetime.
+
+= Premium Features: =
+
+	★ More filter options.
+	
+	★ Option to individual csv for entries.
+
+	★ Additional settings to controls how the contact form 7 data will be storage.
+
+	★ Option to disable specific contact form 7 plugin's form from storing.
+
+	★ GDPR Compatible Settings to make plugin gdpr ready.
+
+	★ More options for report generation.
+	
+	★ More upcoming Upgrade for pro version.
+
+= Premium upgrade =
+
+For premium upgrade please go [here](https://accesspressthemes.com/wordpress-plugins/contact-form-7-store-to-db/)
+
+= Some Useful Links =
+* <strong>Support Forum Link</strong>: http://accesspressthemes.com/support/
+* <strong>Website Link</strong>: http://accesspressthemes.com/
+* <strong>Youtube channel link</strong>: https://www.youtube.com/watch?v=TjZNcVG3fDE
+* <strong>Facebook link</strong>: https://www.facebook.com/AccessPressThemes 
+
+
+
+For a easy installation guide checkout the Installation tab above.
+For complete information and documentation regarding plugin, please visit below links.
+
+[Plugin information](https://accesspressthemes.com/wordpress-plugins/contact-form-7-store-to-db-lite/)
+
+[Docs](http://accesspressthemes.com/documentation/contact-form-7-store-to-db-lite/)
+
+[Demo](http://demo.accesspressthemes.com/wordpress-plugins/contact-form-7-store-to-db-lite/)
+
+[Browse More WordPress Resources](http://wpall.club/)
+
+== Installation ==
+1. Unzip contact-form-7-store-to-db-lite.zip
+2. Upload all the files to the /wp-content/plugins/contact-form-7-store-to-db-lite/
+3. Activate the plugin through the 'Plugins' menu in WordPress.
+4. For customizing the plugin's settings, click on CF7 Entries in WordPress left admin menu.
+
+== Frequently Asked Questions ==
+= What does this plugin do? =
+This plugin provides the ability to allow anyone to store and manage contact form 7 entries for the WordPress
+
+= Where can I find the documentation for the plugin? =
+Once you install the plugin , you can check some general detail below the settings for basic info Complete documentation can be found  [here](https://accesspressthemes.com/documentation/contact-form-7-store-to-db-lite/).
+
+== Screenshots ==
+1. Screenshot-1: Landing Page. 
+2. Screenshot-2: All Entries Page.
+3. Screenshot-3: View Full Detail.
+4. Screenshot-4: General Settings. 
+5. Screenshot-5: Entries Report.
+
+== Changelog ==
+
+= 1.0.4 =
+* COmpatibility issue ccheck for the latest version of the contact from 7 and WP.
+
+= 1.0.3 =
+* Entry Report not current number of form found and resolved.
+
+= 1.0.2 =
+* Some error line plugin text domain found in our earlier update and resolved.
+* Added promotional content in readme.txt.
+
+= 1.0.1 =
+* Few code cleanup done and added additional wordpress resources section.
+
+= 1.0.0 =
+* Plugin submitted to http://wordpress.org for review and approval.
+
+== More Resources ==
+Themes Compatible With Our Plugin :
+
+Contact Form 7 Store to DB Lite works best with every WordPress theme. Its even more remarkable when used with popular themes of ours such as AccessPress Lite and AccessPress Parallax.
+
+AND IF THIS PLUGIN HAS IMPRESSED YOU, THEN YOU WOULD ENJOY OUR OTHER PROJECTS TOO. DO CHECK THESE OUT :
+
+[WPAll Club](https://wpall.club/) - A complete WordPress resources club. WordPress tutorials, blogs, curated free and premium themes and plugins, WordPress deals, offers, hosting info and more.
+ 
+[Premium WordPress Themes](https://themeforest.net/user/accesskeys/portfolio) - 6 premium WordPress themes well suited for all sort of websites. Professional, well coded and highly configurable themes for you. 
+
+[Premium WordPress Plugins](https://codecanyon.net/user/accesskeys/portfolio) - 45+ premium WordPress plugins of many different types. High user ratings, great quality and best sellers in CodeCanyon marketplace. 
+
+[AccessPress Themes](https://accesspressthemes.com/) - AccessPress Themes has 50+ beautiful and elegant, fully responsive, multipurpose themes to meet your need for free and commercial basis.
+
+[8Degree Themes](https://8degreethemes.com/) - 8Degree Themes offers 15+ free WordPress themes and 16+ premium WordPress themes  carefully crafted with creativity.
+
+== Upgrade Notice ==
+There is an update available for the Contact Form 7 Store To DB Lite Plugin. Please update to recieve new updates and bug fixes.
